96f7940321a690f75d18af64bbe2bc0d4e861454
Abdul-ahad-git <167305646+Abdul-ahad-git@users.noreply.github.com>
2025-04-28 11:12:27 +0530

    updated vite config to work on render.com

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
   plugins: [react(), tailwindcss(),],
   server: {
     host: "0.0.0.0",
-    port: import.meta.env.PORT || 10000
+    port: process.env.PORT || 10000
   }
 })
